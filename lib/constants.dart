8ba879306f6063e3d9be3ac670bb9fef259157a8
BalaRohanKotni <balarohan.k@gmail.com>
2023-04-13 15:16:02 +0530

    Updated constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,14 @@ TextStyle semiBold18() {
   return const TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 }
 
+//TODO Sync distanceUnits to firestore
 DistanceUnits distanceUnits = DistanceUnits.km;
 
 enum DistanceUnits { km, mi }
+
+Set<String> vehicles = {};
+
+//TODO Sync currentVehicle to firestore
+String currentVehicle = "";
+
+List vehicleTripsData = [];
