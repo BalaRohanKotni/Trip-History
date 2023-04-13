343f263bcc0cfa7478e3590d6a139aa0f6960b75
BalaRohanKotni <balarohan.k@gmail.com>
2023-04-13 15:16:01 +0530

    Renamed class in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Trip History",
-      home: Signin_Screen(),
+      home: SigninScreen(),
     );
   }
 }
