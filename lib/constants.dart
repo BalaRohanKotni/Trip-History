dedc82684ca69beb37189fa48baea97e91a10864
BalaRohanKotni <balarohan.k@gmail.com>
2023-04-13 15:16:02 +0530

    Updated constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 TextStyle semiBold18() {
   return const TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 }
+
+DistanceUnits distanceUnits = DistanceUnits.km;
+MileageUnits mileageUnits = MileageUnits.kml;
+
+enum DistanceUnits { km, mi }
+
+enum MileageUnits { kml, mpg }
