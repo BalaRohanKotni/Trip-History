5679e49e73bcf3670f733261887e277ca00b05ab
BalaRohanKotni <balarohan.k@gmail.com>
2023-10-17 00:35:21 +0530

    Removed unnecessary imports in home_screen.dart

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/scheduler.dart';
